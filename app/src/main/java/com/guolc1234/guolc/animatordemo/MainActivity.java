f11b6467bd859fba0e5b2e518fe86e97bbb45d4c
guolc <guolc@mapbar.com>
2018-03-22 10:56:50 +0800

    demo picture

--- app/src/main/java/com/guolc1234/guolc/animatordemo/MainActivity.java
+++ app/src/main/java/com/guolc1234/guolc/animatordemo/MainActivity.java
@@ -2,32 +2,31 @@ package com.guolc1234.guolc.animatordemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     BarChartView barChartView1;
     BarChartView barChartView2;
-    BarChartView barChartView3;
-    BarChartView barChartView4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         barChartView1 = findViewById(R.id.cccc);
         barChartView2 = findViewById(R.id.dddd);
-
+        findViewById(R.id.tv).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                barChartView1.animateStart(2000);
+                barChartView2.animateStart(2000);
+            }
+        });
 
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        barChartView1.post(new Runnable() {
-            @Override
-            public void run() {
-                barChartView1.animateStart(2000);
-                barChartView2.animateStart(2000);
-            }
-        });
+
     }
 }
